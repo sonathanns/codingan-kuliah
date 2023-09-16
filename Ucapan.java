c208056f9862ffd5dd39856e6a72b5c2afdc6bc9
Jason Nathaniel <145203983+sonathanns@users.noreply.github.com>
2023-09-16 20:39:31 +0800

    Add files via upload

---
+++ Ucapan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package salam;
+
+/**
+ *
+ * @author jason
+ */
+public class Ucapan {
+    public static void main(String[] args){
+       
+        System.out.println("Halo ULM");
+        System.out.println("ULM Berdaya Saing dan Unggul di Bidang Lahan Basah");
+        System.out.println("Prodi Ilkom ULM Unggul dengan Teknologi Kekinian");
+    
+  }
+}
